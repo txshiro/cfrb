23a5934e8d15f31f1b13134009f98c50a867a020
txshiro <62722282+txshiro@users.noreply.github.com>
2020-04-27 15:52:15 +0200

    fixing stuff

--- commands/Fun/8ball/8ball-example-code.js
+++ commands/Fun/8ball/8ball-example-code.js
@@ -17,17 +17,18 @@ bot.on('message', message =>{
 	//Checking if the message is "!8ball"
 	if (message.content === '!8ball') {
 
-        //Checking first argument
-        if(!args[0]) return message.reply("You need to ask something");
-        
-        //You can add more answers here
-        let answers = ["No, I don't think so.", 'Maybe.','Yes.', '100%.','Totally yes.',"I don't know.",'What is that question ?','Leave me alone.','Ask your parents .'];
-        
-        //Picking random asnwer
-        const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
-        
-        //sending it
-        message.reply(randomAnswer);
+		//Checking first argument
+		if(!args[0]) return message.reply("You need to ask something");
+		if(message.mentions.members.first()) return message.reply("Tagging people is strictly prohibited in this command.")
+
+		//You can add more answers here
+		let answers = ["No, I don't think so.", 'Maybe.','Yes.', '100%.','Totally yes.',"I don't know.",'What is that question ?','Leave me alone.','Ask your parents .'];
+
+		//Picking random asnwer
+		const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
+
+		//sending it
+		message.reply(randomAnswer);
     }
 
 })
